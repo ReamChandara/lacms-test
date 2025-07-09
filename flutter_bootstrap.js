79d3851de33ef29fde1a72f9117aa17e55b26a84
ream chandara <reamchandara@reams-MacBook-Pro.local>
2025-07-09 14:15:12 +0700

    last-commit-09/07/25

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"149ac495838e08a115c42e1882f6a313876cd8
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "765995727"
+    serviceWorkerVersion: "1606731671"
   }
 });
